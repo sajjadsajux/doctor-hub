0823443fba574f30d244ca0049117c85b91b90ea
Sajjad Saju <148693286+sajjadsajux@users.noreply.github.com>
2025-04-22 02:58:58 +0600

    ErrorPage Setup Done

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -9,7 +9,7 @@ const Blog = ({ blog }) => {
       <div className="bg-white rounded-3xl py-10 my-5 md:max-w-3/4 w-full mx-auto text-start p-10  ">
         <div className="space-y-5">
           <div>
-            <h3 className="font-bold text-3xl">{question}</h3>
+            <h3 className="font-bold md:text-3xl">{question}</h3>
             <hr className=" border-dashed border-t-2 border-gray-300 w-full mx-auto mt-3" />
           </div>
           <div>
